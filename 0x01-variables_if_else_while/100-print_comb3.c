2c89e37a5b98c0813330938db81f44558903c4f0
diasbouk <boukdsaid@gmail.com>
2023-09-08 17:06:11 +0100

    task 102

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,15 +11,15 @@ int main(void)
 	int i, j;
 		for (i = 48; i <= 56; i++)
 		{
-			for (j = 49; j <= 57; j++)
+			for (j = i++; j <= 57; j++)
 			{
-				if (i != j || j != 0)
-				{
-					putchar(i);
+					putchar(i);	
 					putchar(j);
-					putchar(44);
-					putchar(32);
-				}
+						if (i != 56 || j != 57)
+						{
+							putchar(44);
+							putchar(32);
+						}
 			}
 		}
 			putchar(10);
