dec96492f5078b8f680b9f82a33d1099600d3519
diasbouk <boukdsaid@gmail.com>
2023-09-08 15:38:18 +0100

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main-The main function
+* Description: THe description
+ * void: no args
+ * printf:Prints the string
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i;
+		for (i = 48; i <= 57; i++)
+		{
+			putchar(i);
+		}
+	putchar(10);
+	return (0);
+}
