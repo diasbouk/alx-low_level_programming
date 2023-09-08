44b54015794d920dc86c26a92212847c5c68d64d
diasbouk <boukdsaid@gmail.com>
2023-09-08 15:47:07 +0100

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main-The main function
+* Description: THe description
+ * void: no args
+ * printf:Prints the string
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i, j;
+		for (i = 48; i <= 57; i++)
+		{
+			putchar(i);
+		}
+		for (j = 97; j <= 102; j++)
+		{
+			putchar(j);
+		}
+			putchar(10);
+			return (0);
+}
