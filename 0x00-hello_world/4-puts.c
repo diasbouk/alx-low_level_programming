bc45fc3de5a1bdffae1f6baa17cca8926bd5f433
diasbouk <boukdsaid@gmail.com>
2023-09-08 01:19:40 +0100

    task 4 is finally done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - Is the main function of our programm
+ * @void: Function does not take any parameters
+ * Description:This is the first programm of C
+ * Return: Returns 0 if the programm works
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
