7472ef26f1d0a9ce7395dfa21d11288a83422c1c
diasbouk <boukdsaid@gmail.com>
2023-09-08 01:28:51 +0100

    task 5 printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main-The main function
+ * Description: THe description
+ * void: no args
+ * printf:Prints the string
+ * Return: returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
