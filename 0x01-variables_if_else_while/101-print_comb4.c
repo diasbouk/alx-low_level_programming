b2b3fffd2524673adcbc578ff7c0a49a9c6720bb
diasbouk <boukdsaid@gmail.com>
2023-09-08 20:38:22 +0100

    task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+* main-The main function
+* Description: THe description
+ * void: no args
+ * printf:Prints the string
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 49; j < 58; j++)
+		{
+			for (k = 50; k < 58; k++)
+			{
+				if (k > j && j > i)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != 55 || j != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
