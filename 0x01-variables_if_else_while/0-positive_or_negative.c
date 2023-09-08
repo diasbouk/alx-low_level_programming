020cd23e477b0b3f4e36aee1abdca2ff23d4fced
diasbouk <boukdsaid@gmail.com>
2023-09-08 11:53:06 +0100

    Task 0 | fixed a bug ( Zero condition ) .

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,9 +18,9 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 		printf("%d is positive\n", n);
-	else if (n)
-		printf("%d is zero\n", n);
-	else if (n > 0)
+	else if (n < 0)
 		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
 	return (0);
 }
