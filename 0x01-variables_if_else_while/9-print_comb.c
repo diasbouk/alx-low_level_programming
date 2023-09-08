fff8379723ee4edb1b8f5c7efca641ede3d30def
diasbouk <boukdsaid@gmail.com>
2023-09-08 15:57:20 +0100

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main-The main function
+* Description: THe description
+ * void: no args
+ * printf:Prints the string
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i, j;
+		for (i = 48; i <= 57; i++)
+		{
+			putchar(i);
+			putchar(44);
+			putchar(32);
+		}
+			putchar(10);
+			return (0);
+}
