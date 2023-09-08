7004d3e621b9ef25aa35d68bb8100291fe8fed43
diasbouk <boukdsaid@gmail.com>
2023-09-08 01:42:13 +0100

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *  main-The main function
+ *  Description: THe description
+ *  void: no args
+ *  puts: prints the content
+ *  Return: returns 0
+  */
+int main(void)
+{
+	puts("Size of a char: 1 byte(s)\n");
+	puts("Size of an int: 4 byte(s)\n");
+	puts("Size of a long int: 4 byte(s)\n");
+	puts("Size of a long long int: 8 byte(s)\n");
+	puts("Size of a float: 4 byte(s)\n");
+	return (0);
+}
