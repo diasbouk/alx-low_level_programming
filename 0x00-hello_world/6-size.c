36a47daffce3f448f4f43d46e5a1d49699fafa62
diasbouk <boukdsaid@gmail.com>
2023-09-08 01:43:16 +0100

    task 6.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
   */
 int main(void)
 {
-	puts("Size of a char: 1 byte(s)\n");
-	puts("Size of an int: 4 byte(s)\n");
-	puts("Size of a long int: 4 byte(s)\n");
-	puts("Size of a long long int: 8 byte(s)\n");
-	puts("Size of a float: 4 byte(s)\n");
+	puts("Size of a char: 1 byte(s)");
+	puts("Size of an int: 4 byte(s)");
+	puts("Size of a long int: 4 byte(s)");
+	puts("Size of a long long int: 8 byte(s)");
+	puts("Size of a float: 4 byte(s)");
 	return (0);
 }
