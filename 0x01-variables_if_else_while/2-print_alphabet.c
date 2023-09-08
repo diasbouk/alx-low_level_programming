6e75aac426086b0c0118df91282ea642430a8c30
diasbouk <boukdsaid@gmail.com>
2023-09-08 15:05:38 +0100

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-The main function
+ * Description: THe description
+ * void: no args
+ * putchar:prints one charachter each time.
+ * printf:Prints the string
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i;
+		for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
