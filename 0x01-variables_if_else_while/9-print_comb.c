2a9388c40348fcbd1f80abbc350f2686977eee4f
diasbouk <boukdsaid@gmail.com>
2023-09-08 15:59:32 +0100

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,8 +12,9 @@ int main(void)
 		for (i = 48; i <= 57; i++)
 		{
 			putchar(i);
-			putchar(44);
-			putchar(32);
+			if (i != 57)
+				putchar(44);
+				putchar(32);
 		}
 			putchar(10);
 			return (0);
