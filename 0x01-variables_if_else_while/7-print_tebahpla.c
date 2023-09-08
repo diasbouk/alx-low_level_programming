cf05f6e257afb3b86cb686760268beaade863536
diasbouk <boukdsaid@gmail.com>
2023-09-08 15:43:19 +0100

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main-The main function
+* Description: THe description
+ * void: no args
+ * printf:Prints the string
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i;
+		for (i = 122; i >= 97; i--)
+		{
+			putchar(i);
+		}
+			putchar(10);
+			return (0);
+}
