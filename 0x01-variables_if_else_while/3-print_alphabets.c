7e8c96f390fbeff6c2b73ce9c089b85ea1a80c90
diasbouk <boukdsaid@gmail.com>
2023-09-08 15:23:15 +0100

    task 3 abcdABCD

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - The main function
+* Description: THe description
+ * void: no args
+ * printf:Prints the string
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i, j;
+		for (i = 97; i <= 122; i++)
+		{
+		putchar(i);
+		}
+		for (j = 65; j <= 90; j++)
+		{
+		putchar(j);
+		}
+		putchar(10);
+			return (0);
+}
