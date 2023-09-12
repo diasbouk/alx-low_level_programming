4226e8b783c8ab6a2d58747fe3390f815cde4f08
diasbouk <boukdsaid@gmail.com>
2023-09-12 04:33:00 +0100

    task 0 of new project

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main-The main function
+* Description: THe description
+ * void: no args
+ * printf:Prints the string
+ * Return: returns 0
+ */
+int main(void)
+{
+	puts("_putchar\n");
+	return (0);
+}
