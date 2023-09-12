03f5e4184a6e718c5c52065e616a82e79ec2b886
diasbouk <boukdsaid@gmail.com>
2023-09-12 14:26:11 +0100

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main-The main function
+* Description: THe description
+ * void: no args
+ * printf:Prints the string
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,9 @@
-// main.h
+#include <stdio.h>
+	void print_alphabet(void)
+	{
+		int i = 97;
+		putchar(i);
+		i++;
+		if (i <= 122)
+		print_alphabet();
+	}
