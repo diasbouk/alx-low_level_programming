a620d5c50ecfde95643d548c3d40637662d2b1db
diasbouk <boukdsaid@gmail.com>
2023-09-13 01:38:50 +0100

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - check the code
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int count, i, j;
+		count = 0;
+		for (i = 0; i <= 9; i++)
+		{
+			if (i >= 0)
+			{
+				printf("0,");
+			}
+			for (j = 0; j < 9; j++)
+			{
+				count = count + i;
+					if (count <= 9)
+                                {
+                                        printf(" ");
+                                }
+					printf(" %d", count);
+				if (j == 8)
+				{
+					printf("$");
+				}
+			}
+			count = 0;
+			printf("\n");
+		}
+}
