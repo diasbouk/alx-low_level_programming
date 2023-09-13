c083e904f282fa9e3eec4a62392c651a450c5e3d
diasbouk <boukdsaid@gmail.com>
2023-09-13 01:39:54 +0100

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,7 +22,7 @@ void times_table(void)
                                 {
                                         printf(" ");
                                 }
-					printf(" %d", count);
+					printf(" %d,", count);
 				if (j == 8)
 				{
 					printf("$");
