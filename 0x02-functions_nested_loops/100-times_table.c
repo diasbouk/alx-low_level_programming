37ac71265ce2ff23e99665ad96635916400a631c
diasbouk <boukdsaid@gmail.com>
2023-09-13 03:02:33 +0100

    task 11

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,36 +7,40 @@
  */
 void print_times_table(int n)
 {
-        int count, i, j;
-                count = 0;
-		if (n <= 15)
+	int count, i, j;
+		count = 0;
+		if (n <= 15 && n > 0)
 		{
-                for (i = 0; i <= n; i++)
-                {
-                        if (i >= 0)
-                        {
-                                printf("0,");
-                        }
-                        for (j = 0; j < n; j++)
-                        {
-                                count = count + i;
-                                        if (count <= 9)
-                                        {
-                                                printf(" ");
-                                        }
-					if(count < 100)
+			for (i = 0; i <= n; i++)
+			{
+				if (i >= 0)
+				{
+					printf("0,");
+				}
+				for (j = 0; j < n; j++)
+				{
+					count = count + i;
+					if (count <= 9)
+					{
+						printf(" ");
+					}
+					if (count < 100)
 					{
 						putchar(' ');
 					}
-                                        printf(" %d", count);
-                                        if (j < (n - 1))
-                                        {
-                                                printf(",");
-                                        }
-                        }
-                        count = 0;
-                        printf("\n");
-                }
+					printf(" %d", count);
+					if (j < (n - 1))
+					{
+						printf(",");
+					}
+				}
+				count = 0;
+				printf("\n");
+			}
+		if (n == 0)
+		{
+			printf("0\n");
+		}
 		}
 }
 
