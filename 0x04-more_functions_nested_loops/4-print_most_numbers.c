5b0ab87e21c1798d40eb52a0a1336479936b8ffc
diasbouk <boukdsaid@gmail.com>
2023-09-14 17:05:10 +0100

    task 5

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers- check the code
+ * I lied again
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int i;
+		for (i = 48; i <= 57; i++)
+		{
+		if (i != 50 && i != 52)
+		{
+			putchar(i);
+		}
+		 putchar(10);
+		}
+}
