7e5a4573a2e89b7018db7980563650a78b84901b
diasbouk <boukdsaid@gmail.com>
2023-09-14 23:54:43 +0100

    task 6 - Print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int i;
+		if (n > 0)
+		{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		}
+		_putchar('\n');
+}
