9a880d59447b689b9bb6e063b6120c5a43be467b
diasbouk <boukdsaid@gmail.com>
2023-09-14 17:13:40 +0100

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the code
+ * lying again
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int i, j, k;
+		for (i = 48; i <= 57; i++)
+		{
+			for (j = 48; j <= 57; j++)
+			{
+				putchar(j);
+			}
+			for (k = 49; k <= 52; k++)
+			{
+				putchar(48);
+				putchar(k);
+			}
+				putchar(10);
+		}
+}
