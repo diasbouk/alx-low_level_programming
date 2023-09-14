e92efd8ea1cc582b5f6247979c500dcb7a7bf65b
diasbouk <boukdsaid@gmail.com>
2023-09-14 16:29:05 +0100

    task 0 - IsUpper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - check the code.
+ * @c: parameter
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+int _isupper(int c);
+#endif
+
