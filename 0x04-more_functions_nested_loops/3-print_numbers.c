c8a68c4a6b4322c307eeed285d348924a2578489
diasbouk <boukdsaid@gmail.com>
2023-09-14 16:43:55 +0100

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ * I lied again buhahahahah
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int i;
+		for (i = 48; i <= 58; i++)
+		{
+			_putchar(i);
+			_putchar(10);
+		}
