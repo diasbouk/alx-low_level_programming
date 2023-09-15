a943716c49eac043fe7308052e11e30f4ebf9bf9
diasbouk <boukdsaid@gmail.com>
2023-09-15 17:00:04 +0100

    ts 100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,18 +8,16 @@
  */
 int main(void)
 {
-    long int num, prime;
-
-    num = 612852475143;
-    for (prime = 2; prime <= num; prime++)
-    {
-        if (num % prime == 0)
-        {
-            num /= prime;
-            prime--;
-        }
-    }
-    printf("%ld\n", prime);
-    return (0);
-	return (0);
+	long int num, prime;
+		num = 612852475143;
+	for (prime = 2; prime <= num; prime++)
+	{
+		if (num % prime == 0)
+		{
+			num /= prime;
+			prime--;
+		}
+	}
+		printf("%ld\n", prime);
+		return (0);
 }
