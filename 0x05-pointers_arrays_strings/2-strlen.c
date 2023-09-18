b55d3c1b623fea8ed5123a18fff8a3ccbd8dd18f
diasbouk <boukdsaid@gmail.com>
2023-09-18 18:26:58 +0100

    task 2 - strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strlen - check the code
+ * @s: parameter string
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	return (sizeof(*s) / sizeof(char));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,4 +6,5 @@
 #include <ctype.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 #endif
