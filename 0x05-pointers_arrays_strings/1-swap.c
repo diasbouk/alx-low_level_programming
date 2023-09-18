bcea4552e62af6635169442cd3df914f1de052bb
diasbouk <boukdsaid@gmail.com>
2023-09-18 18:16:57 +0100

    task 1 -betty

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,13 +3,13 @@
 /**
  * swap_int - check the code
  *@a: num a
- @b: num b
+ *@b: num b
  * Return: Always 0.
  */
 void swap_int(int *a, int *b)
 {
 	int c;
-	c = *b;
-	*b = *a;
-	*a = c;
+		c = *b;
+		*b = *a;
+		*a = c;
 }
