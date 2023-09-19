c025c9762b1c44e96f148221e615ec72c6c0edb9
diasbouk <boukdsaid@gmail.com>
2023-09-19 23:53:59 +0100

    task 4 -rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ *@s: par
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int i;
+		i = 0;
+		while(s[i])
+		{
+			i++;
+		}
+		while (i >= 0)
+		{
+			_putchar(s[i]);
+			i--;
+		}
+			_putchar('\n');
+}
