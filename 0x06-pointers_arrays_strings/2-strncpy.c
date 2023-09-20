2365f9aa2bc9189cb8097a9b1432df327e879510
diasbouk <boukdsaid@gmail.com>
2023-09-20 18:42:02 +0100

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strncpy - check the code
+ * @dest:  dest
+ * @src: src
+ * @n:  n
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (strncpy(dest, src, n));
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,4 +10,5 @@ void _puts(char *str);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 #endif
