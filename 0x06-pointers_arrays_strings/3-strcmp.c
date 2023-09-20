fe695e1483b0bca2cf9007013b72a9f300e07279
diasbouk <boukdsaid@gmail.com>
2023-09-20 18:48:29 +0100

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
+ * _strcmp - check the code
+ * @s1: s1
+ * @s2: s2
  * Return: Always 0.
  */
 int _strcmp(char *s1, char *s2)
 {
 	return (strcmp(s1, s2));
+}
