e3ba8300129c787f45e0d2f2abb8aa318b49bbe6
diasbouk <boukdsaid@gmail.com>
2023-09-20 15:02:00 +0100

    task 0 - Appending

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <string.h>
+int _strlen(char *s);
+void _puts(char *str);
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+#endif
