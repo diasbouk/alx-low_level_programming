52f9efc44e4f7009ac8eb5aa7879b5097ae4a107
diasbouk <boukdsaid@gmail.com>
2023-09-20 01:23:23 +0100

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - check the code
+ * @str:par
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int i, j, n;
+		i = 0;
+		while (str[i])
+		{
+			i++;
+		}
+		j = (i + 1) / 2;
+		if ((i + 1) % 2 == 0)
+		{
+			while (str[j])
+			{
+				j++;
+				_putchar(str[j]);
+			}
+		}
+		n = (i - 1) / 2;
+		if ((i + 1) % 2 != 0)
+		{
+			while (str[n + 1])
+			{
+				n++;
+				_putchar(str[n]);
+			}
+		}
+		_putchar('\n');
+}
