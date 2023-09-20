b3281a3aa4fcda86945143a6a9d5f7be534fe182
diasbouk <boukdsaid@gmail.com>
2023-09-20 01:39:07 +0100

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,15 +9,12 @@
 void print_array(int *a, int n)
 {
 	int i;
-		i = 0;
-		while (i < n)
-		{
-			printf("%d",(a[i]));
-			if ( i != (n - 1))
-			{
-				printf(" ,");
-			}
-			i++;
-		}
+		for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
+	}
 		printf("\n");
 }
