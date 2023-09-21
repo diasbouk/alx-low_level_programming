51141c5a378f00b5825d54d9eaede09feeaee8bd
diasbouk <boukdsaid@gmail.com>
2023-09-21 13:26:36 +0100

    task 5 - DONE

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,10 +8,14 @@
 char *string_toupper(char *str)
 {
 	int i;
-		for (i = 0; str[i] != '\0'; i++)
+		i = 0;
+		while (str[i])
 		{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] = (int)str[i] - 32;
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = (int)str[i] -32;
+			}
+			i++;
 		}
 			return (str);
 }
