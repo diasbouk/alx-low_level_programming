412bf100c694c0c3a1530d7437f98251d568753e
diasbouk <boukdsaid@gmail.com>
2023-09-21 13:35:10 +0100

    task 6 -DONE

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *cap_string - Function to cpitalize strings .
+ * @str: string to be cspitalized
+ * Return: returns the strign capitalized .
+ */
+char *cap_string(char *str)
+{
+int i;
+		i = 0;
+		while (str[i])
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				if (str[i - 1] == '\n')
+				{
+				str[i] = (int)str[i] - 32;
+				}
+			}
+			i++;
+		}
+			return (str);
+}
