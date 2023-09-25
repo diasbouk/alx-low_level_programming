b3f909b9b8818ce2c25c7e65a4937c5666ae75d9
diasbouk <boukdsaid@gmail.com>
2023-09-25 21:42:29 +0100

    task 3 - strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,18 +12,18 @@ int i, j, count;
 i = 0;
 j = 0;
 count = 0;
-while (s[i] != '\0')
+while (s[i] != '\0' && s[i] != ' ')
 {
 while (accept[j] != '\0')
 {
 if (accept[j] == s[i])
 {
 count++;
-break;
 }
 j++;
 }
 i++;
+j = 0;
 }
 return (count);
 }
