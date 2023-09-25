cb0e67683061002df98c504c2190106dca5b7e2a
diasbouk <boukdsaid@gmail.com>
2023-09-25 21:54:55 +0100

    task 4-strpbrk function

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - check the code
+ * @s: string
+ * @accept: second string to compare with
+ * Return: Always 0.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i, j;
+char *ret;
+i = 0;
+j = 0;
+while (s[i] != '\0')
+{
+while (accept[j] != '\0')
+{
+    j++;
+if (s[i] == accept[j])
+ret = &s[i];
+break;
+}
+i++;
+}
+return (ret);
+}
