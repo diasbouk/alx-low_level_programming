9c0d814ad6be76e143aa8e2bf9440d0af50583ce
diasbouk <boukdsaid@gmail.com>
2023-09-25 14:42:43 +0100

    task 2 - StrChr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+/**
+ * _strchr - check the code
+ * @s: string
+ * @c: character to be pointed
+ * Return: Always 0.
+ */
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+return (s);
+}
