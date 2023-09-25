cb0261f7f3a5405038eb39c13c9dee831cd627d7
diasbouk <boukdsaid@gmail.com>
2023-09-25 15:37:12 +0100

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,7 +10,8 @@ char *_strchr(char *s, char c)
 {
 while (*s != '\0')
 {
-if (*s == c) return ((char *) s);
+if (*s == c) 
+return ((char *) s);
 s++;
 }
 return (NULL);
--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-    char *s = "heysaodnv fwopqnfo woifqPOFQIFQE";
+    char *s = "hello";
     char *f;
 
     f = _strchr(s, 'l');
