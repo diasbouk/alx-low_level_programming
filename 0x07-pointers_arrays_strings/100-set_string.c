96276688d9ad743fc86e1cc7be47ba043fc87f32
diasbouk <boukdsaid@gmail.com>
2023-09-26 01:44:45 +0100

    task 100 - SetString

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - check the code
+ * @to: to
+ * @s: s
+ * Return: Always 0.
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,19 +9,16 @@
 void print_diagsums(int *a, int size)
 {
 int i, j, sum;
-sum = 0;
-int *b;
-b = a[0];
 for (i = 0; i < size; i++)
 {
-a[i] = *b;
-for (j = 0; j < size; j++)
-{
-if (i == j)
-{
-sum = sum + b[j];
-}
-}
+    for (j = 0; j < size; j++)
+    {
+        if (i == j)
+        {
+            sum = sum + (*(a + j));
+        }
+        a++;
+    }
 }
 printf("%d", sum);
 }
