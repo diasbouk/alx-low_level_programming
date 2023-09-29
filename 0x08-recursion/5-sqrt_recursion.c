82eb80d9e0de45c232036d3ba039feedcd85bd77
diasbouk <boukdsaid@gmail.com>
2023-09-29 17:36:25 +0100

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * sqrtTwo - sqrt of 2
+ * @c: c
+ * @d: d
+ *
+ * Return: 1
+ */
+int sqrtTwo(int c, int d)
+{
+	if (d * d == c)
+		return (d);
+	else if (d * d > c)
+		return (-1);
+	return (sqrt2(c, d + 1));
+}
+/**
+ * _sqrt_recursion - returns n square
+ * @n: num prmtr
+ *
+ * Return: On success 1.
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrtTwo(n, 1));
+}
