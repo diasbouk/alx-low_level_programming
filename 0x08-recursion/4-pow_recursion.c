3f65cda9547953571f07b9450e6938496c572f87
diasbouk <boukdsaid@gmail.com>
2023-09-29 17:29:00 +0100

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - check the code
+ * @x : x
+ * @y : y
+ * Return: Always 0.
+ */
+int _pow_recursion(int x, int y)
+{
+if (y > 0)
+{
+return (x * _pow_recursion(x, y - 1));
+}
+else if (y == 0)
+{
+return (1);
+}  
+}
