d567a2b09609d58f49a000349430e78eaed227cc
diasbouk <boukdsaid@gmail.com>
2023-09-29 22:03:19 +0100

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,12 +9,12 @@ int is_prime_number(int n)
 {
 if (n > 2 && n % is_prime_number(n - 1) != 0)
 {
-return (1);
+return (0);
 }
 else if (n == 1 || n == 2)
 {
-return (1);
+return (0);
 }
 else
-return (0);
+return (1);
 }
