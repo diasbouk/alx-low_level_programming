c0be971f298b69ef6c0945834a3a8f9c89f58fac
diasbouk <boukdsaid@gmail.com>
2023-09-29 16:15:27 +0100

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,11 +1,22 @@
 #include "main.h"
 
 /**
- *  - check the code
- *
+ * _print_rev_recursion - check the code
+ * @s : str
  * Return: Always 0.
  */
 void _print_rev_recursion(char *s)
 {
-
+int i;
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+while (i >= 0)
+{
+putchar(s[i]);
+i--;
+}
+putchar(10);
 }
