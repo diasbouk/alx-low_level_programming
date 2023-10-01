e77211d83573d2fe258c26dcf87dc4801e7be77d
diasbouk <boukdsaid@gmail.com>
2023-10-01 16:52:02 +0100

    task 100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,20 +2,17 @@
 
 /**
  * palindrome - check the code
- *@s: s
- @findex: index 0
- @lindex: index l
+ * @s: s
+ * @findex: index 0
+  * @lindex: index l
  * Return: Always 0.
  */
-int palindrome (char *s, int findex, int lindex)
+int palindrome(char *s, int findex, int lindex)
 {
 if (findex >= lindex)
-{
 return (1);
-}
-else if (findex == lindex) {
-palindrome(s, findex +1, lindex - 1);
-}    
+else if (findex == lindex)
+palindrome(s, findex + 1, lindex - 1);
 else
 return (0);
 return (0);
@@ -26,9 +23,12 @@ return (0);
  * @s: str
  * Return: Always 0.
  */
-int is_palindrome (char *s)
+int is_palindrome(char *s)
 {
 int length = strlen(s);
-if (length == 0 || length == 1) return (1);
+if (length == 0 || length == 1)
+{
+return (1);
+}
 return (palindrome(s, 0, length - 1));
 }
