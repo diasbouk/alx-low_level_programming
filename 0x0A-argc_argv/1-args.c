94d3495a2473a118831fd045ba6a590ce015bbe0
diasbouk <boukdsaid@gmail.com>
2023-10-02 19:04:04 +0100

    task 0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: numbre of commands
+ * @argv: Commands array
+ * Return: Always 0.
+ */
+int	main(int argc, char **argv)
+{
+printf("%d\n", argc);
+return (0);
+}
