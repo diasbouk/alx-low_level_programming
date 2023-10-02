5fcc57324f9d9d0d14570491eeb0824004640c55
diasbouk <boukdsaid@gmail.com>
2023-10-02 16:57:06 +0100

    task 0

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,6 @@
+#include "main.h"
+int _putchar(char c)
+{
+	putchar(c);
+	return (0);
+}
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,8 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
