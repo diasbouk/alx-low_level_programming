f109ec2f34b57c06da60754188d242c71053c3d9
diasbouk <boukdsaid@gmail.com>
2023-10-02 18:52:47 +0100

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,12 +3,12 @@
 
 /**
  * main - check the code
- * argcto: numbre of commands
+ * @argcto: numbre of commands
  * @argv: Commands array
  * Return: Always 0.
  */
 int	main(int argc, char **argv)
 {
 printf("./%s\n", argv[argc - 1]);
-return 0;
+return (0);
 }
