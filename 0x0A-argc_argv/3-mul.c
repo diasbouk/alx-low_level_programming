b24dce662a977a637d2c561024791a5a8ba13290
diasbouk <boukdsaid@gmail.com>
2023-10-02 22:31:43 +0100

    task 3 - Mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ * @argc: numbre of commands
+ * @argv: Commands array
+ * Return: Always 0.
+ */
+int	main(int argc, char *argv[])
+{
+int i;
+i = 0;
+if (argc != 3)
+{
+printf("Error");
+}
+else
+i = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", i);
+
+return (0);
+}
