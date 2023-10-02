775bf819bd5e9b30ebd4d91ce106f81b6995d7a8
diasbouk <boukdsaid@gmail.com>
2023-10-02 22:55:19 +0100

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,12 +11,21 @@
 int	main(int argc, char *argv[])
 {
 int i, count;
+i = 1;
 count = 0;
 if (argc)
+{
+    if (atoi(argv[i]))
 {
 for (i = 1; i < argc; i++)
 {
 count = count + atoi(argv[i]);
+}
+}
+else
+{
+printf("Error\n");
+return (1);
 }
 }
 printf("%d\n", count);
