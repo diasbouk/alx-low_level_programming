2f8f8b640358230da53bde5891825c82fdaf4153
diasbouk <boukdsaid@gmail.com>
2023-10-03 12:39:10 +0100

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,13 +11,13 @@ char *create_array(unsigned int size, char c)
 {
 unsigned int i = 0;
 char *ptr = malloc(size);
-if (ptr == 0)
+if (size == 0)
 {
 return (NULL);
 }
-if (size == 0)
+if (ptr == 0)
 {
-return (ptr);
+return (NULL);
 }
 while (i <= size)
 {
