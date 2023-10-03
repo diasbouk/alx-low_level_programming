723c757210a2723756bf0c28d638baba35a50248
diasbouk <boukdsaid@gmail.com>
2023-10-03 22:36:28 +0100

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,7 +12,6 @@ int main(int argc, char *argv[])
 {
 int i, j, length, sum;
 char *ptr;
-
 if (argc < 2)
 printf("0\n");
 else
@@ -22,7 +21,6 @@ for (i = 1; i < argc; i++)
 {
 ptr = argv[i];
 length = strlen(ptr);
-
 for (j = 0; j < length; j++)
 {
 if (isdigit(*(ptr + j)) == 0)
@@ -31,10 +29,8 @@ printf("Error\n");
 return (1);
 }
 }
-
 sum += atoi(argv[i]);
 }
-
 printf("%d\n", sum);
 }
 return (0);
