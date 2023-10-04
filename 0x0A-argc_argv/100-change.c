c357740156c58673b014c54f3886ad9be8374cbd
diasbouk <boukdsaid@gmail.com>
2023-10-04 18:39:57 +0100

    Coins changing handled

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,12 +9,40 @@
  */
 int	main(int argc, char *argv[])
 {
+int money = atoi(argv[1]);
+int coins = 0;
 if (argc)
 {
 if (argv[2] || !argv[1])
 {
 printf("Error\n");
 }
+while (money >= 25)
+{
+coins++;
+money = money - 25; 
+}
+while (money >= 10)
+{
+coins++;
+money = money - 10; 
+}
+while (money >= 5)
+{
+coins++;
+money = money - 5; 
+}
+while (money >= 2)
+{
+coins++;
+money = money - 2; 
+}
+while (money >= 1)
+{
+coins++;
+money = money - 1; 
+}
+printf("%d\n", coins);
 }
 return (0);
 }
