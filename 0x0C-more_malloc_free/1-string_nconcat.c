181edcf185f28c567e01d6c2e24942a4981cb53a
diasbouk <boukdsaid@gmail.com>
2023-10-06 21:18:11 +0100

    task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+		if ((sizeof(s2) / sizeof(char)) >= n )
+			ptr = malloc(sizeof(s1) + (n * sizeof(char)));
+		else
+			ptr = malloc(sizeof(s1) + (sizeof(char) * n));
+		if (ptr == NULL)
+		{
+			return (NULL);
+		}
+		if (s1 == NULL || s2 == NULL)
+			return (NULL);
+		*ptr = strcat(s1, s2, n);
+		return (ptr);
+}
