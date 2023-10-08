2b6e5f566f9bd006a096a9a734cf6b2e012c0eef
diasbouk <boukdsaid@gmail.com>
2023-10-09 00:58:27 +0100

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,11 +10,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 char *ptr;
 if (s1 == NULL)
 {
-s1 = " ";
+s1 = "";
 }
 if (s2 == NULL)
 {
-s2 = " ";
+s2 = "";
 }
 if (n <= strlen(s2))
 {
@@ -30,5 +30,4 @@ return (NULL);
 }
 ptr = strncat(s1, s2, n);
 return (ptr);
-free(ptr);
 }
