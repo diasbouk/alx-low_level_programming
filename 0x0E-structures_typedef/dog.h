dbeb108b74bad0d9f5d3329c1c83cacbe29c9b35
diasbouk <boukdsaid@gmail.com>
2023-10-10 16:56:18 +0100

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef HEADER
+#define HEADER
+
+/**
+ * struct dog - struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
