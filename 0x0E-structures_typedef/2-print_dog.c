3acfdc4e64a7237da8f5c525117687d70acdc7a4
diasbouk <boukdsaid@gmail.com>
2023-10-10 21:26:58 +0100

    task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - check the code
+ * @d: pointer to structer
+ * Return: Always 0.
+ */
+void print_dog(struct dog *d)
+{
+	if (d->name)
+		printf("Name: %s\n", d->name);
+	else
+		printf("(nil)\n");
+	if (d->age)
+		printf("Age: %f\n", d->age);
+	else
+		printf("(nil)\n");
+	if (d->owner)
+		printf("Owner: %s\n", d->owner);
+}
