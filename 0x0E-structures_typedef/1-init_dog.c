292c960773b369451ce2a1d3f260a2a462498527
diasbouk <boukdsaid@gmail.com>
2023-10-10 18:06:31 +0100

    task 1 init dawg

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - check the code
+ * @d:dog
+ * @name:name
+ * @age:age
+ * @owner:owner
+ * Return: Always 0.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
