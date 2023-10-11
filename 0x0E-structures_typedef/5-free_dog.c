3f752381c1a595a345c36fb4a6a3b2c3c7f59351
diasbouk <boukdsaid@gmail.com>
2023-10-11 22:41:31 +0100

    5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -7,6 +7,17 @@
  */
 void free_dog(dog_t *d)
 {
+	if (d->owner != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+	if (d->name != NULL)
+	{
+		free(d->name);
+		free(d);
+	}
 	if (d != NULL)
 		free(d);
 }
