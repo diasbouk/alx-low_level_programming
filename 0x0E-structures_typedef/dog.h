daa861736087035d4de2f261bed0f13578622bd7
diasbouk <boukdsaid@gmail.com>
2023-10-11 14:38:04 +0100

    task 1111

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,11 +2,18 @@
 #define HEADER
 /**
  * struct dog - struct
+ * struct dog_t - new type
  * @name: name
  * @age: age
  * @owner: owner
  */
 struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+struct dog_t
 {
 	char *name;
 	float age;
