24608e04070e853eee172af4334abe2fe48ebedd
diasbouk <boukdsaid@gmail.com>
2023-10-12 18:19:48 +0100

    betty .

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,8 +6,8 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-    unsigned int i;
-    int count = 0;
+unsigned int i;
+int count = 0;
 va_list nums;
 va_start (nums, n);
 for (i = 0; i< n; i++)
