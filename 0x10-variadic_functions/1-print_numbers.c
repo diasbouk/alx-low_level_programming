5317607a04215ad58c05f9260495fe4f95255d5a
diasbouk <boukdsaid@gmail.com>
2023-10-12 23:11:07 +0100

    task 0

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,17 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - Function to print numbers
+ * @separator
+ * @n: number of args to add .
+ * Return: sum of all arguments
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+va_list n;
+for (i = 0; i < n; i++)
+{
+
+}
+
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,4 +3,5 @@
 #include <stdio.h>
 #include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
