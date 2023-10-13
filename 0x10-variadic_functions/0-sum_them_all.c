75b20b28e627c4a5fe858e58c6cb635f569e6e0b
diasbouk <boukdsaid@gmail.com>
2023-10-13 18:56:47 +0100

    task 0 include

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,10 +13,9 @@ va_list arg;
 unsigned int i = 0;
 unsigned int count = 0;
 va_start(arg, n);
-while (i < n)
+for (i = 0; i < n; i++)
 {
 count += va_arg(arg, int);
-i++;
 }
 va_end(arg);
 return (count);
