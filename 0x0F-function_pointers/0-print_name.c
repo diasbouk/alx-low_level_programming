bf398d9bbd144064d27eed213838feef2b20ed0f
diasbouk <boukdsaid@gmail.com>
2023-10-13 16:30:21 +0100

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: function to point
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -1,1 +1,1 @@
-0x0F-function_pointers
+# 0x0F-function_pointers
