58e4b5ed620bea33771d16b89eac56e34e83ae3a
diasbouk <boukdsaid@gmail.com>
2023-10-13 15:41:19 +0100

    sum all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,17 +9,14 @@ int sum_them_all(const unsigned int n, ...)
 unsigned int i;
 unsigned int count = 0;
 va_list nums;
-if (n == 0)
-{
-return (0);
-}
-else
-{
 va_start(nums, n);
 for (i = 0; i < n; i++)
 {
 count = count + va_arg(nums, int);
 }
 return (count);
+if (n == 0)
+{
+return (0);
 }
 }
