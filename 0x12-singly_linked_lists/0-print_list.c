a4370f597d6d87b6a86b8c6eb59b312d2748a7be
diasbouk <boukdsaid@gmail.com>
2023-10-19 22:46:14 +0100

    if h exitst

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,20 +7,23 @@
 size_t print_list(const list_t *h)
 {
 	int count = 0;
-		if (h->str == NULL)
+		if (h)
 		{
-			printf("[0] (nil)\n");
-			count++;
+			if (h->str == NULL)
+			{	
+				printf("[0] (nil)\n");
+					count++;
+			}
+			else if (h->str != NULL)
+			{
+				printf("[%d] %s\n", h->len, h->str);
+				count++;
+			}
 		}
-		else if (h->str != NULL)
-		{
-			printf("[%d] %s\n", h->len, h->str);
-			count++;
-		}
-		if (h->next != NULL)
-		{
-			count++;
-			print_list(h->next);
-		}
-		return (count);
+			if (h->next != NULL)
+			{
+				count++;
+					print_list(h->next);
+			}
+			return (count);
 }
