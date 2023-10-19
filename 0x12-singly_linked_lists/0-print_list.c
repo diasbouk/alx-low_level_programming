cb592aa8f2c6d66aebcef7da67401a5a5f4b6305
diasbouk <boukdsaid@gmail.com>
2023-10-19 15:26:04 +0100

    task 00

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,21 +6,20 @@
 size_t print_list(const list_t *h)
 {
 	int count = 0;
-	if (h->str != NULL)
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n");
+		count++;
+	}
+	else if (h->str != NULL)
 	{
 		printf("[%d] %s\n", h->len, h->str);
 		count++;
-	}	
-	if (h->next != NULL && h->str != NULL)
-	{
-		count++;
-		print_list(h->next);
 	}
-	else if(h->str == NULL)
+	if (h->next != NULL)
 	{
-		printf("[0] (nil)\n");
-		print_list(h->next);
 		count++;
+		print_list(h->next);
 	}
 	return (count);
 }
