7a2f271d04563c8d1b0d33f8e9e4358f11faa83e
diasbouk <boukdsaid@gmail.com>
2023-10-20 00:14:21 +0100

    task 0

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -11,11 +11,10 @@
 size_t list_len(const list_t *h)
 {
 	int count = 0;
-                while (h)
-                {
-                h = h->next;
-                count++;
-                }
-        return (count);
-
+		while (h)
+		{
+			h = h->next;
+			count++;
+			}
+		return (count);
 }
