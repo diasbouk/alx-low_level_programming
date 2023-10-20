27e3e6c1028707bcce7f56a4184f5ad50401d9a3
diasbouk <boukdsaid@gmail.com>
2023-10-20 01:38:47 +0100

    TASK 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,10 +10,10 @@
 void free_list(list_t *head)
 {
 	list_t *tmp;
-	while((tmp= head) != NULL)
-	{
-		head = head->next;
-		free(tmp->str);
-		free(tmp);
-	}
+		while ((tmp = head) != NULL)
+		{
+			head = head->next;
+			free(tmp->str);
+			free(tmp);
+		}
 }
