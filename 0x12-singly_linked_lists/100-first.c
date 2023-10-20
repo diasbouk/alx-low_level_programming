8474a4ad57b906f3479e18dbd287eb3aa291ddae
diasbouk <boukdsaid@gmail.com>
2023-10-20 12:27:35 +0100

    TASK 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,5 +9,5 @@ void before_main(void) __attribute__((constructor));
 void before_main(void)
 {
 	printf("A tortoise, having pretty good sense of a hare's nature,");
-	printf("challenges one to a race.)\n");
+	printf("challenges one to a race.\n");
 }
