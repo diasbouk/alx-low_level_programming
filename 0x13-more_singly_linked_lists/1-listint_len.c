5d3781566730c1f34a35a952c076a45c8b775dbe
diasbouk <boukdsaid@gmail.com>
2023-10-23 17:44:25 +0100

    BETTY

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint_len - function to print && return number of nodes .
+ * listint_len - function to print && return number of nodes .
  *  @h: pointer to the first node of list .
  * Return: number of nodes .
  */
