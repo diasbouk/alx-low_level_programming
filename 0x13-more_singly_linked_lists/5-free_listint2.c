da2d7d1bb7029c46e63dd7aea3455959c3373d09
diasbouk <boukdsaid@gmail.com>
2023-10-23 20:03:34 +0100

    TAKS 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,9 +11,9 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
-	while ((temp = *head) != NULL)
-	{
-		*head = (*head)->next;
-		free(temp);
-	}
+		while ((temp = *head) != NULL)
+		{
+			*head = (*head)->next;
+			free(temp);
+		}
 }
