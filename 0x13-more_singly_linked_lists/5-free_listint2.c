c4c3b7a1e6f6e267e6a10df22b9928a5e473735f
diasbouk <boukdsaid@gmail.com>
2023-10-23 19:58:56 +0100

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - check the code
+ * @head: poiner to pointer to head
+ * Dosent return anything (void .)
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	while ((temp = *head) != NULL)
+	{
+		*head = (*head)->next;
+		temp = *head;
+		free(temp);
+	}
+}
