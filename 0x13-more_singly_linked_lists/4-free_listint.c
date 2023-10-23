6f3a8d6c958b6c1d03ecc0294e164bdad3af025b
diasbouk <boukdsaid@gmail.com>
2023-10-23 18:08:34 +0100

    TASK 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint - Adds new node t=at the first of a list
+ * @head: pointer to list head adress
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *fr;
+	while (head = fr)
+	{
+
+		free(fr)
+		head = head->next;
+	}
+}
