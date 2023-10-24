55fae6b09449faedda0c3f2ad61f02e3a64e2b9b
diasbouk <boukdsaid@gmail.com>
2023-10-24 11:49:23 +0100

    FREE

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,10 +11,13 @@
 int pop_listint(listint_t **head)
 {
 	int data;
+	listint_t *tmp;
 		if (*head == NULL)
 			return (0);
 		data = (*head)->n;
+		tmp = *head;
 		*head = (*head)->next;
+		free(tmp);
 		return (data);
 }
 
