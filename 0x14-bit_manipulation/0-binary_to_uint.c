4cc4bcfb804e0abfa7a762928008f36d185c6266
diasbouk <boukdsaid@gmail.com>
2023-10-27 00:50:20 +0100

    Rm

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,8 @@
+#ifndef BINARY
+#define BINARY
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+unsigned int binary_to_uint(const char *b);
+#endif
