e23d969267245aeb382935250e2c343f15a9cfe7
diasbouk <boukdsaid@gmail.com>
2023-10-27 01:05:36 +0100

    TASK 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,4 +7,28 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int num;
+	int lent, base_2;
 
+	if (!b)
+		return (0);
+
+	ui = 0;
+
+	for (lent = 0; b[lent] != '\0'; lent++)
+		;
+
+	for (lent--, base_2 = 1; lent >= 0; lent--, base_2 *= 2)
+	{
+		if (b[lent] != '0' && b[lent] != '1')
+		{
+			return (0);
+		}
+
+		if (b[lent] & 1)
+		{
+			num += base_2;
+		}
+	}
+
+	return (num);
