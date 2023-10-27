2768c78a50f118067b3944361f2a526907de2804
diasbouk <boukdsaid@gmail.com>
2023-10-27 01:18:52 +0100

    TASK 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,10 +11,10 @@ void print_binary(unsigned long int n)
 	{
 		if (n >> 1)
 			print_binary(n >> 1);
-		_putchar((n & 1) + '0');
+		putchar((n & 1) + '0');
 	}
 	else
 	{
-		_putchar('0');
+		putchar('0');
 	}
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,4 +5,5 @@
 #include <string.h>
 #include <ctype.h>
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 #endif
