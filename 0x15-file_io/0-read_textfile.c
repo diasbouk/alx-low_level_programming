f6fc1ef973652c92e3cecabf0debb2732bb5e23c
diasbouk <boukdsaid@gmail.com>
2023-10-30 19:06:38 +0100

    TASK 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * read_textfile - 
+ *
+ * Return: Always 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,8 @@
+#ifndef FILES
+#define FILES
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
