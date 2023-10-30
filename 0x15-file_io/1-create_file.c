275e9e6068217ca643812aea155b7fd244f166cc
diasbouk <boukdsaid@gmail.com>
2023-10-31 00:28:01 +0100

    TASK 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * create_file - function to create a file
+ * @filename : you know
+ * @text_content : NULL terminated string to put in file
+ * Return: 1 on success , -1 otherwise
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, file_text;
+		if (!filename)
+			return (-1);
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+		if (fd < 0)
+			return (-1);
+		file_text = write(fd, text_content, strlen(text_content));
+		if (file_text == -1)
+			return (-1);
+		return (1);
+}
+
