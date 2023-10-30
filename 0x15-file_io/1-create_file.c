9129997ba6d27789a5bf50541242845c88ee0721
diasbouk <boukdsaid@gmail.com>
2023-10-31 00:30:18 +0100

    TASK 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,6 +17,7 @@ int create_file(const char *filename, char *text_content)
 		file_text = write(fd, text_content, strlen(text_content));
 		if (file_text == -1)
 			return (-1);
+		close(fd);
 		return (1);
 }
 
