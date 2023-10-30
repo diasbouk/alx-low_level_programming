f256a7e6fd97da86245ee0f127b3efa31055352e
diasbouk <boukdsaid@gmail.com>
2023-10-31 00:42:20 +0100

    TASk 3

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * append_text_to_file - Appends to a file
+ * @filename: as always
+ * @text_content: appended
+ * Return: 1 || -1 (You know)
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, num_ret;
+
+	if (!filename)
+		return (-1);
+	if (!text_content)
+		text_content = "\0";
+	fd = open(filename, O_WRONLY);
+	if (fd == -1)
+		return (-1);
+	num_ret = write(fd, text_content, strlen(text_content));
+	if (num_ret == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
