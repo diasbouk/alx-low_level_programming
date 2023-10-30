fcc6de72158f390c52616d85c113aa95b89609cf
diasbouk <boukdsaid@gmail.com>
2023-10-31 00:58:31 +0100

    TASK 100

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * main - Main
- * Return: 0 or -1 .
+ * elf_header - elf
  */
-int main(void)
+void elf_header(void)
 {
 	puts("time wasnt enough :( \n");
-	return (0);
 }
---
+++ 0x15-file_io/100-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - Main
+ * Return: 0
+ */
+int main(void)
+{
+	elf_header();
+	return (0);
+}
