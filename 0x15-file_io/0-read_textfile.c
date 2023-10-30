e5ec20a310ed31d83b58ac3259f800439834299a
diasbouk <boukdsaid@gmail.com>
2023-10-31 00:09:15 +0100

    TASK 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,11 +1,28 @@
 #include "main.h"
 
 /**
- * read_textfile - 
- *
+ * read_textfile -  function to read and print content of a file
+ * @filename : name of file
+ * @letters : number of letters to reead from file
  * Return: Always 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-
+	size_t number_of_letters;
+	size_t memory_num;
+	int file_ret;
+	char *buffer;
+		if (filename == NULL)
+			return (0);
+		buffer  = malloc(letters);
+		if (buffer == NULL)
+			return (0);
+		file_ret = open(filename, O_RDONLY);
+		if (file_ret < 0 )
+			return (0);
+		memory_num = read(file_ret, buffer, letters);
+		number_of_letters = write(STDOUT_FILENO, buffer, memory_num);
+		free(buffer);
+		close(file_ret);
+		return (number_of_letters);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,5 +4,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
