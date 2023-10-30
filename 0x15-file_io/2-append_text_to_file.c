161311af9c30c815acd5f06ce2aa6f78cc42f524
diasbouk <boukdsaid@gmail.com>
2023-10-31 00:47:01 +0100

    TASk 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	num_ret = write(fd, text_content, strlen(text_content));
+	if (text_content)
+		num_ret = write(fd, text_content, strlen(text_content));
 	if (num_ret == -1)
 		return (-1);
 	close(fd);
