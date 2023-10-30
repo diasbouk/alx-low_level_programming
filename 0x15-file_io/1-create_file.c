5d37c895c4123dae81510fa795717e75ea82b865
diasbouk <boukdsaid@gmail.com>
2023-10-31 00:33:18 +0100

    TASK 11

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 		if (text_content == NULL)
 			*text_content = '\0';
 		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-		if (fd < 0)
+		if (fd == -1)
 			return (-1);
 		file_text = write(fd, text_content, strlen(text_content));
 		if (file_text == -1)
