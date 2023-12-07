6e26832654958d91af1f0bb43a8918fe65b3de97
diasbouk <boukdsaid@gmail.com>
2023-12-07 13:18:16 +0100

    Task 2 : setting code && cleaning ..

---
+++ 0x17-doubly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - Adds a node as the head of a list
+ * @head: The head of our list
+ * @n: New node's data
+ * Return : Pointer to the new Head
+*/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+    
+}
