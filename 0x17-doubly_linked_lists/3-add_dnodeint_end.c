08d344665d424c70cf1c0621949a93fd644ee9b3
diasbouk <boukdsaid@gmail.com>
2023-12-07 18:12:26 +0100

    Task 3 : Code check

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,5 +8,24 @@
 */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    
+	dlistint_t *last_node = malloc(sizeof(dlistint_t));
+	dlistint_t *temp = *head;
+		
+		if (last_node == NULL)
+			return (NULL);
+		if (*head == NULL)
+		{
+			*head = last_node;
+			return (last_node);
+		}
+		
+		while (temp->next != NULL)
+			temp = temp->next;
+
+		last_node->n = n;
+		temp->next = last_node;
+		last_node->prev = temp;
+		last_node->next = NULL;
+  
+		return (last_node);
 }
