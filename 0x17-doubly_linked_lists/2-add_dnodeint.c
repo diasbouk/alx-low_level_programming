dba7c5a80d9062c131ae430761dbb63943a3c836
diasbouk <boukdsaid@gmail.com>
2023-12-07 14:09:50 +0100

    Task 2 : betty checked . Task done

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -4,20 +4,18 @@
  * add_dnodeint - Adds a node as the head of a list
  * @head: The head of our list
  * @n: New node's data
- * Return : Pointer to the new Head
+ * Return: Pointer to the new Head
 */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
-    new_node->n = n;
-    new_node->prev = NULL;
-    new_node->next = *head;
-    if (*head != NULL)
-        (*head)->prev = new_node;
-    *head = new_node;
-    
-    return (new_node);
-
+dlistint_t *new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return (NULL);
+new_node->n = n;
+new_node->prev = NULL;
+new_node->next = *head;
+if (*head != NULL)
+(*head)->prev = new_node;
+*head = new_node;
+return (new_node);
 }
