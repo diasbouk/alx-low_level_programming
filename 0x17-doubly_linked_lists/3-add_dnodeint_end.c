49d63aaaf80fb7a0a0c977c4e4a3f0fa18d53d06
diasbouk <boukdsaid@gmail.com>
2023-12-07 18:23:44 +0100

    Task 3 : Code Done ..

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,21 +8,27 @@
 */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *last_node = malloc(sizeof(dlistint_t));
-	dlistint_t *temp = *head;
-		if (last_node == NULL)
-			return (NULL);
-		if (*head == NULL)
-		{
-			*head = last_node;
-			free(last_node);
-			return (*head);
-		}
-		while (temp->next != NULL)
-			temp = temp->next;
-		last_node->n = n;
-		temp->next = last_node;
-		last_node->prev = temp;
-		last_node->next = NULL;
-		return (last_node);
+	dlistint_t *new_mem, *last_dir;
+
+	new_mem = malloc(sizeof(dlistint_t));
+
+	if (new_mem == NULL)
+		return (NULL);
+
+	new_mem->n = n;
+	new_mem->next = NULL;
+	new_mem->prev = NULL;
+	if (*head == NULL)
+	{
+		*head = new_mem;
+		return (new_mem);
+	}
+	last_dir = *head;
+
+	while (last_dir->next != NULL)
+		last_dir = last_dir->next;
+	last_dir->next = new_mem;
+	new_mem->prev = last_dir;
+
+	return (new_mem);
 }
