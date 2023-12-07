9251435c6ca39f153621dcabf65b013a2b0a80e4
diasbouk <boukdsaid@gmail.com>
2023-12-07 14:04:32 +0100

    Task 2 : code check

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -20,7 +20,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
     (*head)->prev = new_node;
     new_node->next = *head;
     new_node->prev = NULL;
+    *head = new_node;
     
-    return (new_node);
+    return (*head);
 
 }
