1d75ea38a6239f6e1de565899eb49954bf37cf7f
diasbouk <boukdsaid@gmail.com>
2023-12-07 14:08:35 +0100

    TASK 2 : Code fixed , checking if head isnt null .

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,17 +11,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
     dlistint_t *new_node = malloc(sizeof(dlistint_t));
     if (new_node == NULL)
         return (NULL);
-    if (head == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
     new_node->n = n;
-    (*head)->prev = new_node;
-    new_node->next = *head;
     new_node->prev = NULL;
+    new_node->next = *head;
+    if (*head != NULL)
+        (*head)->prev = new_node;
     *head = new_node;
     
-    return (*head);
+    return (new_node);
 
 }
