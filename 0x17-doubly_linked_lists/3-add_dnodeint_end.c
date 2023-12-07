9405311a439b2abc2e0c64a3dc50029e231b828b
diasbouk <boukdsaid@gmail.com>
2023-12-07 14:12:37 +0100

    Task 3 : initializing stuff

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - Adds a node as the end of a list
+ * @head: The head of our list
+ * @n: New node's data
+ * Return: Pointer to the new element of list
+*/
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+    
+}
