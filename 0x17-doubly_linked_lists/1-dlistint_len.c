e7a28ae7dd93729ef37b3d8ca4f217b27d20af0b
diasbouk <boukdsaid@gmail.com>
2023-12-07 13:14:42 +0100

    Task 1 : Betty style check

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -4,15 +4,15 @@
  * @h: Pointer to the head of the list
  * Return: number of elemnts
  */
- size_t dlistint_len(const dlistint_t *h)
- {
-    int count = 0;
-    if (h == 0)
-        return (0);
-    while (h)
-    {
-        count++;
-        h = h->next;
-    }
-    return (count);
- }
+size_t dlistint_len(const dlistint_t *h)
+{
+int count = 0;
+if (h == 0)
+return (0);
+while (h)
+{
+count++;
+h = h->next;
+}
+return (count);
+}
