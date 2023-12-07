18f1b8850a14bba2415debdfa48a9313b6fa6f52
diasbouk <boukdsaid@gmail.com>
2023-12-07 13:03:29 +0100

    TASK 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,12 +7,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    int count = 0
+    int count = 0;
     if (h == NULL)
-        return (NULL);
+        return (0);
     while (h)
     {
-        print("%d\n", h->n);
+        printf("%d\n", h->n);
+        h = h->next;
         count++;
     }
     
