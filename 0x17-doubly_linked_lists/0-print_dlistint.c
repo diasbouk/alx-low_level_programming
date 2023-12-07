b88d7739da06851d42cfa740ad4e0b3b5dde2289
diasbouk <boukdsaid@gmail.com>
2023-12-07 13:07:44 +0100

    Betty style / task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,15 +7,15 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    int count = 0;
-    if (h == NULL)
-        return (0);
-    while (h)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
-    
-    return (count);
+int count = 0;
+if (h == NULL)
+return (0);
+while (h)
+{
+printf("%d\n", h->n);
+h = h->next;
+count++;
+}
+
+return (count);
 }
