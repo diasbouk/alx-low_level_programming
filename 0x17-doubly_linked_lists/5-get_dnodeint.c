64372b36342a3087854452b5816ceb33f1fc3da2
diasbouk <boukdsaid@gmail.com>
2023-12-08 21:52:47 +0100

    Task 5 : Betty style

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -4,16 +4,17 @@
  * get_dnodeint_at_index - Gets a node at index
  * @head: Head of the list
  * @index: index of our node
+ * Return: Adress of our node or NULL if it dosent exist
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	while (head)
-	{
-		if (i == index)
-			return (head);
-		head = head->next;
-		i++;
-	}
-	return (NULL);
+		while (head)
+		{
+			if (i == index)
+				return (head);
+			head = head->next;
+			i++;
+		}
+		return (NULL);
 }
