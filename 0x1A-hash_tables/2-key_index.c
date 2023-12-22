752e1a841cec4e907665b3863d4143caca83e789
diasbouk <boukdsaid@gmail.com>
2023-12-22 21:19:20 +0100

    Task 2 : Find index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Finds the index of a key
+ * @key: Key of index we're looking for 
+ * @size: size of array of our hash_tables
+ * Return:Returns the index at which the key/value pair should be stored in the array of the hash table
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index = hash_djb2(key) % size;
+		return (index);
+}
