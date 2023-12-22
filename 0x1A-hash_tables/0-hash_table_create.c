ea3da6ff2f7cc0a85a39ac21cad6c18dc39677d1
diasbouk <boukdsaid@gmail.com>
2023-12-22 19:41:33 +0100

    Task 0 : Betty Done , All good

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,16 +3,17 @@
 /**
  * hash_table_create - Function that creates a hash_table
  * @size: The Hash table size
- * Return: A pointer to th new hash_table 
+ * Return: A pointer to the new hash_table
  * or NULL if it failed
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-  hash_table_t *new_table = malloc(sizeof(hash_table_t));
-      if (new_table == NULL)
-        return (NULL);
-      new_table->size = size;
-      new_table->array = malloc(sizeof(char *) * new_table->size);
-  
-      return (new_table);
+	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+		if (new_table == NULL)
+			return (NULL);
+		new_table->size = size;
+
+		new_table->array = malloc(sizeof(char *) * new_table->size);
+
+		return (new_table);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -22,9 +22,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -37,14 +37,11 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 
-/**
- * Implemented fucntions go here 
- */
 
 hash_table_t *hash_table_create(unsigned long int size);
 
