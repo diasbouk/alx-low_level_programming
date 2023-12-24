9dc7547a5b81cebea114b7334592d2a0791796b0
diasbouk <boukdsaid@gmail.com>
2023-12-24 16:21:26 +0100

    Task 100 betty

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,11 +2,11 @@
 
 /**
  * _atoi - Converts a string into an integer
- * @s: String to convert to int 
+ * @s: String to convert to int
  * Return: New integer
 */
 
 int _atoi(char *s)
 {
-return (atoi(s)) ;   
+return (atoi(s));
 }
