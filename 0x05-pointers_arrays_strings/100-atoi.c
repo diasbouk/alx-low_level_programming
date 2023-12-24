82f27b0cea30503c1e564237b356d150bda67afa
diasbouk <boukdsaid@gmail.com>
2023-12-24 16:18:15 +0100

    Task 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string into an integer
+ * @s: String to convert to int 
+ * Return: New integer
+*/
+
+int _atoi(char *s)
+{
+    return (atoi(s)) ;   
+}
