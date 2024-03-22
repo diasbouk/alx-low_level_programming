20f9c7fc409f1a7e8840e1124889ed073b13afd4
diasbouk <boukdsaid@gmail.com>
2024-03-22 01:29:12 +0000

    TASK 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			temp = temp->next;
 			i++;
 		}
-		if (temp->next == NULL)
+		if (temp == NULL)
 			new_node = add_dnodeint_end(h, n);
 		else
 		{
