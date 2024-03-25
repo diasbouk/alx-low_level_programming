a5d7a6fd0d2e9dcb2def3caf44f9aa4018582856
diasbouk <boukdsaid@gmail.com>
2024-03-25 17:32:47 +0000

    Last task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/alx-tools/0x18.c/raw/master/gm
-chmod +x gm
+wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
