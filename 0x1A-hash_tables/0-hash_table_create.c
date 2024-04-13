6fc78d460e5ff1a7a26a7e6e29d05c3659e23b05
diasbouk <boukdsaid@gmail.com>
2024-04-13 10:40:41 +0000

    task one

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 			return (NULL);
 		new_table->size = size;
 
-		new_table->array = malloc(sizeof(char *) * new_table->size);
+		new_table->array = malloc(sizeof(hash_node_t);
 
 		return (new_table);
 }
