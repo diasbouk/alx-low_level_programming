ec6ef8838273baea0725c4d9831e0ff680dd644b
diasbouk <boukdsaid@gmail.com>
2024-08-20 16:25:42 +0100

    Task 0, here and back again to old projects

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "function_pointers.h"
 
 /**
@@ -9,8 +8,5 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-if (name != NULL && f != NULL)
-{
-f(name);
-}
+	(*f)(name);
 }
