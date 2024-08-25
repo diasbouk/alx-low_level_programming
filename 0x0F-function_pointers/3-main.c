966769a2ba89ba572f2b866ba7fc64ce996ac6d7
diasbouk <boukdsaid@gmail.com>
2024-08-25 10:34:21 +0100

    Exit status added , all shall be good now

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,17 +14,17 @@ int main(int ac, char **av)
 	if (ac != 4)
 	{
 		printf("Error\n");
-		return (0);
+		return (98);
 	}
 	if (!get_op_func(av[2]))
 	{
 		printf("Error\n");
-		return (0);
+		return (99);
 	}
 	if ((av[2][0] == '%' || av[2][0] == '/') && atoi(av[3]) == 0)
 	{
 		printf("Error\n");
-		return (0);
+		return (100);
 	}
 	func = get_op_func(av[2]);
 	printf("%d\n", func(atoi(av[1]), atoi(av[3])));
